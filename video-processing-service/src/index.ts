1fba18ab442eb944dd259e22540393c2866627b7
chrisragasa <ragasa.chris@gmail.com>
2025-02-15 12:27:32 -0800

    [ADD] add process-video route

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,31 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const PORT = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+  // Get path of the input video file from the request body
+  const inputVideoPath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputVideoPath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing file path");
+  }
+
+  ffmpeg(inputVideoPath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+      res.status(200).send("Video processing started.");
+    })
+    .on("error", (err) => {
+      console.log(`An error occured: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
-app.listen(PORT, () => {
-  console.log(`Video processing service running at http://localhost:${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Video processing service running at http://localhost:${port}`);
 });
